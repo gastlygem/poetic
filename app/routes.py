3092697eeab008b40c18dbdb277e009fa68cb5f4
Joel Wang <gastlygem@gmail.com>
2020-10-08 02:14:05 +0800

    3.3 url_for

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app.forms import PoemForm
-from flask import flash, redirect, render_template
+from flask import flash, redirect, render_template, url_for
 from app import app
 
 from data import poems, poets
@@ -20,5 +20,5 @@ def add_poem():
     if form.validate_on_submit():
         poems.append({"title": form.title.data, "poem": form.poem.data, "poet_id": int(form.poet.data)})
         flash(f"诗歌《{form.title.data}》添加成功")
-        return redirect('/index')
+        return redirect(url_for('index'))
     return render_template("add_poem.html", title="添加诗", form=form)
